7062d9f4fce577c4cfdd94f5efc288d917e9b683
Katarzyna Sołoducha <soloducha.katarzyna@gmail.com>
2022-01-14 16:50:45 +0100

    scaling buttons

--- src/components/orders/Pagination.js
+++ src/components/orders/Pagination.js
@@ -7,18 +7,141 @@ const Pagination = (props) => {
 
     let pages = []
     const numberOfPages = Math.ceil(props.ordersList.length / 5)
-    for (let number = 1; number <= numberOfPages; number++) {
-        pages.push(
-            <Button 
-                key={number}
-                variant={props.theme === 'light'? "outline-dark" : "outline-light"}
-                onClick={() => props.selectActivePage(number)}
-                active={props.activePage === number}
-            >
-                {number}
-            </Button>
-        );
-      }
+    if(numberOfPages <= 5){
+        for (let number = 1; number <= numberOfPages; number++) {
+            pages.push(
+                <Button 
+                    key={number}
+                    variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                    onClick={() => props.selectActivePage(number)}
+                    active={props.activePage === number}
+                >
+                    {number}
+                </Button>
+            );
+        }
+    } 
+    else {
+        if(props.activePage <= 2){
+            for (let number = 1; number <= 3; number++) {
+                pages.push(
+                    <Button 
+                        key={number}
+                        variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                        onClick={() => props.selectActivePage(number)}
+                        active={props.activePage === number}
+                    >
+                        {number}
+                    </Button>
+                );
+            }
+            pages.push(
+                <Button 
+                    key={'...'}
+                    variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                    active={false}
+                >
+                    ...
+                </Button>
+            );
+            pages.push(
+                <Button 
+                        key={numberOfPages}
+                        variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                        onClick={() => props.selectActivePage(numberOfPages)}
+                        active={props.activePage === numberOfPages}
+                    >
+                        {numberOfPages}
+                </Button>
+            );
+        }
+        else if(props.activePage >= numberOfPages - 1){
+            pages.push(
+                <Button 
+                        key={1}
+                        variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                        onClick={() => props.selectActivePage(1)}
+                        active={props.activePage === 1}
+                    >
+                        1
+                </Button>
+            );
+            pages.push(
+                <Button 
+                    key={'...'}
+                    variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                    active={false}
+                >
+                    ...
+                </Button>
+            );            
+            for (let number = numberOfPages - 2; number <= numberOfPages; number++) {
+                pages.push(
+                    <Button 
+                        key={number}
+                        variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                        onClick={() => props.selectActivePage(number)}
+                        active={props.activePage === number}
+                    >
+                        {number}
+                    </Button>
+                );
+            }
+        }
+        else{
+            pages.push(
+                <Button 
+                        key={1}
+                        variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                        onClick={() => props.selectActivePage(1)}
+                        active={props.activePage === 1}
+                    >
+                        1
+                </Button>
+            );
+            pages.push(
+                <Button 
+                    key={'...1'}
+                    variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                    active={false}
+                >
+                    ...
+                </Button>
+            ); 
+            for (let number = props.activePage - 1; number <= props.activePage + 1; number++) {
+                pages.push(
+                    <Button 
+                        key={number}
+                        variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                        onClick={() => props.selectActivePage(number)}
+                        active={props.activePage === number}
+                    >
+                        {number}
+                    </Button>
+                );
+            }
+            pages.push(
+                <Button 
+                    key={'...2'}
+                    variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                    active={false}
+                >
+                    ...
+                </Button>
+            ); 
+            pages.push(
+                <Button 
+                        key={numberOfPages}
+                        variant={props.theme === 'light'? "outline-dark" : "outline-light"}
+                        onClick={() => props.selectActivePage(numberOfPages)}
+                        active={props.activePage === numberOfPages}
+                    >
+                        {numberOfPages}
+                </Button>
+            );
+        }
+    }
+    
 
       return(
         <div className='pagination' >
